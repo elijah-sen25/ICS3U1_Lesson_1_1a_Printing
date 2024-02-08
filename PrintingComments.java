0a07d8069677a1ba8676f6189d1ec5957b58fcc0
elijah-sen25 <elijah.sen25@ycdsbk12.ca>
2024-02-08 12:47:15 -0500

    Updated PrintingComments java

--- PrintingComments.java
+++ PrintingComments.java
@@ -8,14 +8,26 @@ class PrintingComments extends ConsoleProgram {
   public void run() {
 
     // Say hello
-    
+    System.out.println("Hi");
+    System.out.println("There!");
 
     // Say hello on the same line
-    
-    
-    // multiline comment
-    
-    
+    System.out.print("Hi");
+    System.out.print(" There!");
 
+    // multiline comment
+    /* 
+     * This is
+     * a multiline
+     * comment
+    */
+  System.out.println("");
+    System.out.println("**********");
+    System.out.println("*        *");
+    System.out.println("*        *");
+    System.out.println("*        *");
+    System.out.println("*        *");
+    System.out.println("*        *");
+    System.out.println("**********");
   }
 }
